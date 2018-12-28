3b60651e6297f6d58f2d147e29df104ce099c558
harishm72 <harishm72>
2018-12-28 19:25:43 +0530

    In index.js, refactor using reduce, add devDependencies

--- server/index.js
+++ server/index.js
@@ -14,35 +14,45 @@ var db = mysql.createConnection({
   });
 
 app.use(express.static(path.join(__dirname, '../client')));
-db.connect();
+
 app.get('/', (req, res) => res.send("Home Page!!"));
 app.get('/luckyTeams', (req, res) => {
     db.query("select toss_winner, count(*) as wins From matches GROUP BY toss_winner", (err, results) =>{
-        let out = {};
-        results.map((res) => { out[res['toss_winner']] = res['wins'] })
-        res.send(out)
+        results = results.reduce((acc, data) => { 
+            acc[data['toss_winner']] = data['wins'] 
+            return acc;
+        }, {})
+        res.send(results)
     })
 });
 app.get('/strikeRate', (req, res) => {
     db.query("select batsman, (sum(batsman_runs)/count(*))*100 as strike From deliveries GROUP BY batsman", (err, results) =>{
-        let out ={}
-        results.map((res) => {out[res['batsman']] = res['strike']})
-      res.send(out)
+        results = results.reduce((acc, data) => { 
+            acc[data['batsman']] = data['strike'] 
+            return acc;
+        }, {})
+      res.send(results)
       })
 });
 app.get('/deathOverEcon', (req, res) => {
     db.query("select bowler, (sum(total_runs)/(count(*)/6)) as runs From deliveries where over > 16 GROUP BY bowler;", (err, results) =>{
-        let out ={}
-        results.map((res) => {out[res['bowler']] = res['runs']})
-      res.send(out)
-    })
+        results = results.reduce((acc, data) => { 
+            acc[data['bowler']] = data['runs'] 
+            return acc;
+        }, {})
+      res.send(results)
+      })
 });
 app.get('/scoreInFinal', (req, res) => {
     db.query("select batting_team, sum(total_runs)/count(distinct match_id) as runs from deliveries where match_id in (select max(id) from matches group by season) and over < 7 group by batting_team ", (err, results) =>{
-        let out = {}
-        results.map((res) => {out[res['batting_team']] = res['runs']})
-        res.send(out)
-    })
+        results = results.reduce((acc, data) => { 
+            acc[data['batting_team']] = data['runs'] 
+            return acc;
+        }, {})
+      res.send(results)
+      })
 });
 
-app.listen(port, () => console.log(`I am  listening on port ${port}!`));
+app.listen(port, () => { 
+    db.connect();
+    console.log(`I am  listening on port ${port}!`)});
