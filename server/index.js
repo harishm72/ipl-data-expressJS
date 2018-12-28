ba7cb5f1518d55fb5384b72f4b7be6e56814b251
harishm72 <harishm72>
2018-12-28 19:00:06 +0530

    Refactor using reduce, completely avoid for loops

--- server/index.js
+++ server/index.js
@@ -4,14 +4,17 @@ const path = require('path');
 const port = 3000;
 
 let iplData = require('./src/dataProject.js')
+let matches = require("../data/matches.json")
+let deliveries = require('../data/deliveries.json');
+
 
 app.use(express.static(path.join(__dirname, '../client')));
 
 app.get('/', (req, res) => res.send("Home Page!!"));
-app.get('/luckyTeams', (req, res) => res.send(iplData.luckyTeams()));
-app.get('/strikeRate', (req, res) => res.send(iplData.strikeRate()));
-app.get('/deathOverEcon', (req, res) => res.send(iplData.deathOverEcon()));
-app.get('/scoreInFinal', (req, res) => res.send(iplData.scoreInFinal()))
+app.get('/luckyTeams', (req, res) => res.send(iplData.luckyTeams(matches)));
+app.get('/strikeRate', (req, res) => res.send(iplData.strikeRate(deliveries)));
+app.get('/deathOverEcon', (req, res) => res.send(iplData.deathOverEcon(deliveries)));
+app.get('/scoreInFinal', (req, res) => res.send(iplData.scoreInFinal(matches, deliveries)))
 
 app.listen(port, () => console.log(`I am  listening on port ${port}!`))
 
--- server/src/dataProject.js
+++ server/src/dataProject.js
@@ -1,80 +1,78 @@
-// matches.csv  and deliveries.csv  file
-let matches = require("../data/matches.json");
-let deliveries = require("../data/deliveries.json");
-let fs = require('fs');
 
+let luckyTeams = (matchData) => {
+    let result = matchData.reduce((lucky, match) => {
+        lucky[match['toss_winner']] = lucky[match['toss_winner']] || 0;
+        lucky[match['toss_winner']] += 1;
+        return lucky;
+    }, {})
+    return result;
+}
+let strikeRate = (deliveryData) => {
+    let result = deliveryData.reduce((strike, delivery) => {
 
-// 1. Luckiest team
-let luckyTeams = () => {
+        strike[delivery['batsman']] = strike[delivery['batsman']] || [parseInt(delivery['batsman_runs'], 10), 0];
+        strike[delivery['batsman']] = [strike[delivery['batsman']][0] + parseInt(delivery['batsman_runs'], 10), strike[delivery['batsman']][1] + 1]
+        return strike;
+    }, {})
 
-    let lucky = {}
-    //to get the toss wins for each team
-    for (let key in matches) {
-        if (lucky.hasOwnProperty(matches[key]['toss_winner']))
-            lucky[matches[key]['toss_winner']] += 1;
-        else lucky[matches[key]['toss_winner']] = 1;
-    }
-    //  for (let key in matches) {
-    //      lucky[matches[key]['toss_winner']] = lucky[matches[key]['toss_winner']] || 0;
-    //      lucky[matches[key]['toss_winner']] += 1;
-    //  }
-    return lucky;
-};
-// 2. Strike rate of batsman
-let strikeRate = () => {
+    Object.keys(result).map((key) => {
+        result[key] = result[key].reduce((acc, data) => Number(((acc / data) * 100).toFixed(2)))
+    })
+    return result;
 
-    let strike = {}
-    for (let key in deliveries) {
-        if (!strike.hasOwnProperty(deliveries[key]['batsman']))
-            strike[deliveries[key]['batsman']] = [parseInt(deliveries[key]['batsman_runs'], 10), 1];
-        else strike[deliveries[key]['batsman']] = [(strike[deliveries[key]['batsman']][0] + parseInt(deliveries[key]['batsman_runs'])), (strike[deliveries[key]['batsman']][1] + 1)];
-    }
-    let strikeRate = {};
-    for (let key in strike)
-        strikeRate[key] = ((strike[key][0] / strike[key][1]) * 100).toFixed(2);
+}
+let deathOverEcon = (deliveryData) => {
+    let result = deliveryData.filter((delivery) => Number(delivery['over']) > 16).reduce((ball, delivery) => {
+        ball[delivery['bowler']] = ball[delivery['bowler']] || [Number(delivery['total_runs']), 0];
+        let runs = ball[delivery['bowler']][0] + Number(delivery['total_runs']);
+        let balls = ball[delivery['bowler']][1] + 1
+        ball[delivery['bowler']] = [runs, balls]
+        return ball
+    }, {})
 
-    return strikeRate;
-};
-// 3. performance of bowlers in the death overs(last 4 overs)
-let deathOverEcon = () => {
+    Object.keys(result).map((key) => {
+        result[key] = result[key].reduce((acc, data) => Number(((acc / (data / 6)).toFixed(2))))
+    })
 
-    let deathBowl = {};
-    for (let key in deliveries) {
-        if (Number(deliveries[key]['over']) >= 17) {
-            if (deathBowl.hasOwnProperty(deliveries[key]['bowler']))
-                deathBowl[deliveries[key]['bowler']] += Number(deliveries[key]['total_runs'])
-            else deathBowl[deliveries[key]['bowler']] = Number(deliveries[key]['total_runs'])
-        }
-    }
-    return deathBowl;
-};
-// 4. runs scored by teams in power play (first 6 overs) in final matches in all seasons
-let scoreInFinal = () => {
-    let finalId = {};
+    return result;
 
-    // gets match_ids of final matches -- the last matches in that season
-    for (let key in matches)
-        finalId[matches[key]['season']] = matches[key]['id'];
-    // since we need only ids reassign 'finalId' variable to an array that has only 
-    finalId = Object.values(finalId);
+}
+let scoreinFinal = (matchdata, deliverydata) => {
 
-    let mostRuns = {};
-    for (let key in deliveries) {
-        if (finalId.includes(Number(deliveries[key]['match_id']))) {
-            if (Number(deliveries[key]['over']) <= 6) {
-                if (mostRuns.hasOwnProperty(deliveries[key]['batting_team']))
-                    mostRuns[deliveries[key]['batting_team']] += Number(deliveries[key]['total_runs']);
-                else mostRuns[deliveries[key]['batting_team']] = Number(deliveries[key]['total_runs'])
-            }
-        }
-    }
-    return mostRuns;
-};
+    let finalId = matchdata.reduce((acc, data) =>{
+        acc[data['season']] = acc[data['season']] || [data['id']]
+        acc[data['season']].push(data['id'])
+        return acc;
+    }, {})
+    Object.keys(finalId).map((key) =>{
+        finalId[key] = Math.max(...finalId[key])
+    })
+    finalId = Object.values(finalId)
+    
+    let teams = matchdata.filter((match) => finalId.includes(match['id'])).reduce((teams, data) =>{
+            teams[data['team1']] = teams[data['team1']] || 0;
+                teams[data['team1']] += 1;
+                teams[data['team2']] = teams[data['team2']] || 0;
+                teams[data['team2']] += 1;
+            return teams;
+        },{})
 
+    let scoreinFinal = deliverydata.filter((delivery) => (finalId.includes(Number(delivery['match_id'])) && Number(delivery['over']) < 7))
+        .reduce((final, delivery) => {
+            final[delivery['batting_team']] = final[delivery['batting_team']] || 0;
+            final[delivery['batting_team']] += Number(delivery['total_runs']);
+            return final;
+        }, {})
+
+        Object.keys(scoreinFinal).map((key) =>{
+            scoreinFinal[key] = Number(((scoreinFinal[key] / teams[key])).toFixed(2))
+        })
+        return scoreinFinal
+}
 
 module.exports = {
-    luckyTeams : luckyTeams, 
-    strikeRate : strikeRate, 
-    deathOverEcon : deathOverEcon, 
-    scoreInFinal : scoreInFinal
+    luckyTeams,
+    strikeRate,
+    deathOverEcon,
+    scoreinFinal
 };
